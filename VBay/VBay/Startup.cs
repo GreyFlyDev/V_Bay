5b4043b1d6cf5f15ea543668dfccba188ecd962c
Greg Flynn <gflynn@covdnssrv.co.volusia.fl.us>
2016-05-26 08:41:41 -0400

    Initial Commit

---
+++ VBay/VBay/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VBay.Startup))]
+namespace VBay
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
