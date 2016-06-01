e9cbd8957c782114c226f493bf2c90312982d0b6
Greg Flynn <gflynn@covdnssrv.co.volusia.fl.us>
2016-06-01 10:22:11 -0400

    Added dbContext

---
+++ Vbay2/Vbay2/DAL/VbayContext.cs
@@ -0,0 +1,22 @@
+﻿using Vbay2.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Vbay2.DAL
+{
+    public class VbayContext : DbContext
+    {
+        public VbayContext() : base("VbayContext")
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Ad> Ads { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
